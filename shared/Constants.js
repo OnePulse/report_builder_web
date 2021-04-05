6de1f2244379ff349fdfb0bc0b80fc5a70f158b2
barrywhyteop <barry.whyte@onepulse.com>
2021-04-05 11:54:40 +0100

    Delete Constants.js

--- shared/Constants.js
+++
@@ -1,7 +0,0 @@
-export const constants = {
-    "API": {
-        "BASE_URL"          : "https://data.onepulse.com/v3/api/",
-        "BASE_URL_LEGACY"   : "https://data.onepulse.com/api/"
-    },
-    "APP_TOKEN": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOnsicm9sZSI6WyJsb2dpbioiLCJlbnJvbCoiLCJ2ZXJpZnkqIiwibWV0YSoiLCJ0cmFuc2xhdGUqIl19LCJzdWIiOiJhcHBsaWNhdGlvbiIsImlzcyI6Imh0dHA6XC9cL29uZXB1bHNlLnYyLmFwaSIsImlhdCI6IjE0NDk4MjM3MjUiLCJleHAiOiIxNjA3NTAzNzI1IiwibmJmIjoiMTQ0OTgyMzcyNSIsImp0aSI6ImViYzI4NTgxYWNmZDVkZjQ0ZmUwMGY4NGQzMzA1NDQzIn0.b-kSgigtlWhtZPPQAY6z4vD5eH16pv7eCXnIkSQ9jzc"
-};
